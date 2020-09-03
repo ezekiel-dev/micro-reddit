7abba5cc0b3e54f2303e3e9038c0bbbdc331aeb0
vanheaven-ui <vanheaven6@gmail.com>
2020-09-03 22:10:52 +0300

    Create User  model

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  validates :email, :password, :username, :firstname, :secondname,
+              presence: true,
+              length: { minimum: 5 }
+  validates :email, :password, :username,
+              uniqueness: true
+  validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i }
+  validates :password, 
+              length: { maximum: 20 },
+              confirmation: true
+  validates :password_confirmation, presence: true
+
+end
